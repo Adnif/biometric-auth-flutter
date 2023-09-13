35c7beaa8969885e543b4cadd4f116595af43165
Adnif <ahmadbani9@gmail.com>
2023-09-13 14:47:08 +0700

    finished login and signup API

--- server/src/user/controller.js
+++ server/src/user/controller.js
@@ -15,11 +15,46 @@ const login = (req, res) => {
     const sql = `SELECT * FROM users WHERE email = '${email}' AND password = '${password}'`;
     pool.query(sql, (error, results) => {
         if(error) throw error;
+
+        if(results.rows.length === 0){
+            res.status(409).send('Wrong Credentials');
+            return;
+        }
+
         res.status(200).json(results.rows);
     });
 }
 
+const signUp = async (req, res) => {
+    const username = req.body.username;
+    const email = req.body.email;
+    const password = req.body.password;
+    const device_id = req.body.device_id;
+
+    const emailQuery = `SELECT * FROM users WHERE username = '${username}' OR email = '${email}'`;
+
+    try {
+        const results = await pool.query(emailQuery);
+
+        const isUserExists = results.rows.length > 0;
+
+        if (isUserExists) {
+            res.status(409).send('Username or email already exists.');
+        } else {
+            const signUpQuery = `INSERT INTO users (username, password, email, device_id) VALUES ('${username}', '${password}', '${email}', '${device_id}')`;
+
+            await pool.query(signUpQuery);
+
+            res.status(201).send('User created successfully.');
+        }
+    } catch (error) {
+        throw error;
+    }
+}
+
+
 module.exports = {
     getUsers,
     login,
+    signUp
 };
--- server/src/user/routes.js
+++ server/src/user/routes.js
@@ -4,6 +4,8 @@ const controller = require('./controller');
 const router = Router();
 
 router.get('/', controller.getUsers);
-router.post('/login', controller.login)
+router.post('/login', controller.login);
+router.post('/signup', controller.signUp);
 
 module.exports = router;
+``
